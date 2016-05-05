96014dcc7f98afa2b8699bec27760010f167ab88
Aaron van Geffen <aaron@aaronweb.net>
2016-05-05 13:26:32 +0200

    Use a fallback in case of long conversation names.
    In case the conversation name ends very long due to a large number of participants,
    the file name may end up too long for the operating system to grant a file handle.
    This is particularly true if several member's names have been anonymised, i.e. when
    they are representated by a numerical Facebook user address.
    
    The script will now export these threads to sequential files prefixed with
    "unnamed_conversation".

--- threadsplitter.pl
+++ threadsplitter.pl
@@ -37,6 +37,7 @@ if ($collection->size == 0) {
 
 say "Exporting conversation threads to " . $export_dir . "...";
 my %conversations;
+my $unnamed_conversations = 0;
 for my $conversation ($collection->each) {
 	my @conversation_partners = grep(!/@{[MY_NAME]}/, split(', ', $conversation->text));
 	my $conversation_name = join(', ', @conversation_partners);
@@ -49,8 +50,21 @@ for my $conversation ($collection->each) {
 	}
 
 	open SPLITMSG, ">", $export_dir . "/" . $conversation_name . ".htm";
+	if (tell(SPLITMSG) == -1) {
+		print "Could not open file handle for '", $export_dir . "/" . $conversation_name . ".htm' -- trying '";
+		$conversation_name = "unnamed_conversation_" . ++$unnamed_conversations;
+		say $conversation_name . ".htm";
+
+		open SPLITMSG, ">", $export_dir . "/" . $conversation_name . ".htm";
+		if (tell(SPLITMSG) == -1) {
+			say "Could not open file handle for '", $export_dir . "/" . $conversation_name . ".htm' either -- cowardly exiting.";
+			exit 2;
+		}
+	}
+
 	print SPLITMSG '<!DOCTYPE html><meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
 	print SPLITMSG '<style type="text/css">body { font-family: sans-serif; } .user { font-weight: bold; } .meta { margin-left: 1em; color: #999 } </style>';
+	print SPLITMSG '<p>Participants: ', join(', ', @conversation_partners), '</p>';
 
 	my $messages = $conversation->children->reverse;
 	my $next_is_header = 0;
